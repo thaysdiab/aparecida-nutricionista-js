138e2c05ad2691c614141ab93c6d5e4985ed154b
thaysrq <thayspereirasilva2@gmail.com>
2020-01-20 15:25:01 -0300

    Validar mensagens de erro

--- js/form.js
+++ js/form.js
@@ -10,8 +10,8 @@ botaoAdicionar.addEventListener("click", function(event) {
     var erros = validaPaciente(paciente);
     
     if(erros .length > 0){
-        var mensagemErro =  document.querySelector("#mensagem-erro");
-        mensagemErro.textContent = erro;
+        exibeMensagensDeErros(erro);
+
         return;
     }
 
@@ -19,8 +19,19 @@ botaoAdicionar.addEventListener("click", function(event) {
 
     tabela.appendChild(pacienteTr);
     form.reset();
+    var mensagensErro =  document.querySelector("#mensagens-erro");
+    mensagensErro.innerHTML = "";
 
 });
+function exibeMensagensDeErros(erros){
+     var ul = document.querySelector("#mensagens-erro");
+     ul.innerHTML = "";
+     erros.forEach(function(erro){
+         var li = document.createElement("li");
+         li.textContent = erro;
+         ul.appendChild(li);
+     });
+}
 
 function obtemPacienteDoFormulario(form){
     var paciente = {
@@ -54,8 +65,10 @@ function montaTd(dado, classe){
 
 function validaPaciente(paciente){
     var erros = [];
-
+    if (paciente.nome.length == 0) erros.push ("Nome não pode ficar em branco");
     if(!validaPeso(paciente.peso )) erros.push("Peso é inválido");
     if(!validaAltura(paciente.altura )) erros.push("Altura é inválido"); 
+    if(paciente.gordura.length == 0) erros.push("A gordura não pode ficar em branco");
+    if(paciente.peso.length == 0) erros.push("O peso não pode ficar em branco")
     return erros;
 }
