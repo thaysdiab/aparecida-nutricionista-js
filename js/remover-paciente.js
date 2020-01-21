3b93e108ae62275286e4d0307991eacde3805f9a
thaysrq <thayspereirasilva2@gmail.com>
2020-01-21 17:40:01 -0300

    criar evento ao remover paciente

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,8 +1,17 @@
-var paciente = document.querySelectorAll(".paciente");
+//pacientes.forEach(function(paciente){
+ //   paciente.addEventListener("dblclick",function(){
+ //       this.remove();
+//    });
+//});
 
-pacientes.forEach(function(paciente){
-    paciente.addEventListener("dblclick",function(){
-        this.remove();
+var pacientes = document.querySelectorAll(".paciente");
+var tabela = document.querySelector("table");
+
+    tabela.addEventListener('dblclick', function(event) {
+    
+        // Somente executará o código caso o elemento em que clicamos seja um <td>
+        if (event.target.tagName == 'TD') {
+            event.target.parentNode.remove()
+        }
     });
-});
-
+    
