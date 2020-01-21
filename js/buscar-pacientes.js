ed104431d97fcc144bec71d4fe9c90a693c3cbc3
thaysrq <thayspereirasilva2@gmail.com>
2020-01-21 19:57:57 -0300

    tratar erros de requisição

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -1,15 +1,21 @@
 var botaoAdicionar = document.querySelector("#buscar-pacientes");
 botaoAdicionar.addEventListener("click"),function(){
 
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "api-pacientes.herokuapp.com/pacientes");
-    xhr.addEventListener("load", function(){
-        var reposta = xhr.responseText;
-        var pacientes = JSON.parse(resposta);
-        pacientes.forEach(function(paciente){
-            adicionarPacienteNaTabela(paciente); 
-        });
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "api-pacientes.herokuapp.com/pacientes");
+xhr.addEventListener("load", function(){
+        var erroAjax = document.querySelector("#erro-ajax")
+        if(xhr.status ==200){
+            erroAjax.classList.add("invisivel")
+            var reposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
 
+            pacientes.forEach(function(paciente){
+                adicionarPacienteNaTabela(paciente); 
+            });
+        }else{
+            erroAjax.classList.remove("invisivel");
+        }
     });
     xhr.send();
 }
